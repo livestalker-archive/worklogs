f0d6f219be661a181b59ccd8a303e1e5c966517d
aleksio <14111984Alexey>
2015-10-19 16:44:15 +0300

    list of needs worklogs

--- app/models/worklog_mailer.rb
+++ app/models/worklog_mailer.rb
@@ -5,7 +5,7 @@ class WorklogMailer < Mailer
     @all_users = User.status('1').all
     users_need_worklogs = Array.new
     @all_users.each do |user|
-      worklog = Worklog.where(:user_id => user.id)
+      worklog = Worklog.where(:user_id => user.id).where('DATE(created_at) = ?', @day)
       if worklog.empty?
         users_need_worklogs.push user.id
       end
@@ -14,7 +14,6 @@ class WorklogMailer < Mailer
   end
 
   def check_worklogs(day)
-    #@url = url_for(:controller => 'worklogs', :action => 'index', :day => day)
     @day = day || Date.today.to_s
     get_all_debtors
     recipients = @all_need_worklogs_users.collect(&:mail)
