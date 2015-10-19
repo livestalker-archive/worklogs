ef420a2a2d6a9942623cd93c3e2f09ca55f7ccbc
aleksio <14111984Alexey>
2015-10-19 15:21:39 +0300

    worklogs task template

--- app/models/worklog_mailer.rb
+++ app/models/worklog_mailer.rb
@@ -3,10 +3,19 @@ class WorklogMailer < Mailer
 
   def check_worklogs(day)
     #@url = url_for(:controller => 'worklogs', :action => 'index', :day => day)
-
-    #recipients
-    mail :to =>  'alexey@livestalker.net' ,
+    @all_users = User.status('1').all
+    users_need_worklogs = Array.new
+    @all_users.each do |user|
+      worklog = Worklog.where(:user_id => user.id)
+      if worklog.empty?
+        users_need_worklogs.push user.id
+      end
+    end
+    @all_need_worklogs_users = User.where(id: users_need_worklogs)
+    recipients = @all_need_worklogs_users.collect(&:mail)
+    recipient_manager = 'alexey@livestalker.net'
+    mail :to => recipient_manager,
          :subject => 'Test worklogs'
   end
-  
+
 end
