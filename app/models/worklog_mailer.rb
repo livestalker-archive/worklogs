5b22728b4acb2edee845fad7bd98b06c950951b2
aleksio <14111984Alexey>
2015-10-20 12:36:36 +0300

    version 1.4

--- app/models/worklog_mailer.rb
+++ app/models/worklog_mailer.rb
@@ -24,8 +24,8 @@ class WorklogMailer < Mailer
   def send_all_worklogs(day)
     @day = day || Date.today.to_s
     @worklogs = Worklog.where('DATE(created_at) = ?', @day).order('created_at desc, id desc')
-    mail :to => Setting.plugin_worklogs['WORKLOGS_MAIL_CC'],
+    mail :to => Setting.plugin_worklogs['WORKLOGS_MAIL_GLOBAL'],
+         :cc => Setting.plugin_worklogs['WORKLOGS_MAIL_CC'],
          :subject => 'Day worklogs'
   end
-
 end
