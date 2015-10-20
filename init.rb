34185a72fe9d4d9579aef3c34df689cf6c5fe97f
aleksio <14111984Alexey>
2015-10-20 10:03:33 +0300

    config page

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :worklogs do
   name 'Worklogs plugin'
   author 'Alexey V. Grebenshchikov'
   description ''
-  version '0.1.0'
+  version '0.1.3'
   url ''
   author_url ''
 
